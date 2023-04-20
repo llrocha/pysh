7ce1656316750c7147dc399a74ca86c8c10b4c0c
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2023-04-20 18:10:54 -0300

    Command launcher

---
+++ pysh_launcher.py
@@ -0,0 +1,17 @@
+import os
+
+
+class launcher():
+    def launch_cmd(self, arguments):
+        if(len(arguments)):
+            pid = os.fork()
+            if pid == 0:
+                print('\nchild process, pid:', os.getpid())
+                #print(f'{arguments}!')
+                os.system(' '.join(arguments))
+            elif pid > 0:
+                print('parent process, pid:', os.getpid())
+                os.waitpid(pid, os.WNOHANG)
+                print('child process finalized, pid:', pid)
+            else:
+                print(f'error creating child process {arguments}!')
