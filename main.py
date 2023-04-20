d46ab44f6583c76150e8af14612556c76bf320ce
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2023-04-20 18:20:21 -0300

    Change pylint suggestions

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
+"""
+Shell interpreter written in Python
+"""
+
 from pysh_builtins import builtins
 from pysh_launcher import launcher
 
 
 PATH='/bin'
-prompt = '> '
+PROMPT = '> '
 builtin_cmds = builtins()
 cmd_launcher = launcher()
 
 
-def debug_cmd(arguments):
-    if(len(arguments)):
-        print(f'COMMAND: {arguments[0]}')
-        for i, arg in enumerate(arguments[1:]):
-            print(f'ARGUMENT[{i}]: {arg}')
 
 def execute_cmd(arguments):
-    if(len(arguments)):
-        if builtin_cmds.is_builtin(arguments):
-            builtin_cmds.launch_builtin(arguments)
+    if len(arguments):
+        if builtin_cmds.is_builtin(arguments=arguments):
+            builtin_cmds.launch_builtin(arguments=arguments)
         else:
-            cmd_launcher.launch_cmd(arguments)
+            cmd_launcher.launch_cmd(arguments=arguments)
 
 
 if __name__ == '__main__':
-    while(True):
-        line = input(prompt)
+    while True:
+        line = input(PROMPT)
         arguments = line.split()
-        execute_cmd(arguments)
+        execute_cmd(arguments=arguments)
