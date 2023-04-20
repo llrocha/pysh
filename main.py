1f32f6c4749e8c9abb53ffdd0fe514eae34013b0
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2023-04-20 18:11:24 -0300

    Remove unused functions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import os
-
 from pysh_builtins import builtins
+from pysh_launcher import launcher
+
 
 PATH='/bin'
 prompt = '> '
 builtin_cmds = builtins()
+cmd_launcher = launcher()
 
 
 def debug_cmd(arguments):
@@ -13,40 +14,16 @@ def debug_cmd(arguments):
         for i, arg in enumerate(arguments[1:]):
             print(f'ARGUMENT[{i}]: {arg}')
 
-def launch_cmd(arguments):
-    if(len(arguments)):
-        pid = os.fork()
-        if pid == 0:
-            print('\nchild process, pid:', os.getpid())
-            #print(f'{arguments}!')
-            os.system(' '.join(arguments))
-        elif pid > 0:
-            print('parent process, pid:', os.getpid())
-            os.waitpid(pid, os.WNOHANG)
-            print('child process finalized, pid:', pid)
-        else:
-            print(f'error creating child process {arguments}!')
-
-def is_builtin(arguments):
-    return arguments[0].strip() in builtin_cmds
-
-def launch_builtin(arguments):
-    command = arguments[0].strip()
-    if command == 'exit':
-        if len(arguments) > 1:
-            exit_value = int(arguments[1])
-        else:
-            exit_value = 0
-        exit(exit_value)
-
 def execute_cmd(arguments):
     if(len(arguments)):
         if builtin_cmds.is_builtin(arguments):
             builtin_cmds.launch_builtin(arguments)
         else:
-            launch_cmd(arguments)
+            cmd_launcher.launch_cmd(arguments)
 
-while(True):
-    line = input(prompt)
-    arguments = line.split()    
-    execute_cmd(arguments)
+
+if __name__ == '__main__':
+    while(True):
+        line = input(prompt)
+        arguments = line.split()
+        execute_cmd(arguments)
