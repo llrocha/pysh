9ef051bf584094ba6e7d747880974d6583adaa0c
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2023-04-22 22:48:29 -0300

    Use subprocess instead of os.system

--- pysh_launcher.py
+++ pysh_launcher.py
@@ -1,17 +1,11 @@
-import os
+import subprocess
 
 
 class CommandLauncher():
     def launch_cmd(self, arguments):
         if len(arguments):
-            pid = os.fork()
-            if pid == 0:
-                print('\nchild process, pid:', os.getpid())
-                #print(f'{arguments}!')
-                os.system(' '.join(arguments))
-            elif pid > 0:
-                print('parent process, pid:', os.getpid())
-                os.waitpid(pid, os.WNOHANG)
-                print('child process finalized, pid:', pid)
-            else:
-                print(f'error creating child process {arguments}!')
+            process = subprocess.Popen(arguments)
+            pid = process.pid
+            print(f'child process, pid: {pid}')
+            process.wait()
+            print(f'child process finalized, pid: {pid}')
