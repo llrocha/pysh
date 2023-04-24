58603f396db69647ed196d2a2233e3973266affd
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2023-04-23 22:40:37 -0300

    Adding support to execute python script

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 """
 Shell interpreter written in Python
 """
+import ast
 
 from pysh_builtins import BuiltinLauncher
 from pysh_launcher import CommandLauncher
 
 
-PATH='/bin'
-PROMPT = '> '
+PATH = '/bin'
+PROMPT = '$ '
+COMMAND = ''
 builtin_launcher = BuiltinLauncher()
 cmd_launcher = CommandLauncher()
 
+
+def run_python(arguments):
+    exec(' '.join(arguments))
+
+
+def is_valid_python_call(arguments):
+    try:
+        tree = ast.parse(arguments)
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Call):
+                return True
+        return False
+    except SyntaxError:
+        return False
 
 
 def execute_cmd(arguments):
     if len(arguments):
         if builtin_launcher.is_builtin(arguments):
             builtin_launcher.launch_builtin(arguments)
+        elif is_valid_python_call(' '.join(arguments)):
+            run_python(arguments)
         else:
             cmd_launcher.launch_cmd(arguments)
 
@@ -24,9 +42,17 @@ def execute_cmd(arguments):
 if __name__ == '__main__':
     while True:
         try:
+            print(COMMAND)
             line = input(PROMPT)
-            execute_cmd(line.split())
+            if line[-1:] != '\\':
+                COMMAND += line
+                execute_cmd(COMMAND.split())
+                COMMAND = ''
+            else:
+                COMMAND += line[:-1]
         except NotImplementedError as e:
             print(f'{e} is not implemented yet')
+        except KeyboardInterrupt as e:
+            print(e)
         except Exception as e:
             print(e)
