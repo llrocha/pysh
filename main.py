61dfdce2b7aefa584ad3f462c30a126f4ebc1200
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2023-04-23 22:50:19 -0300

    Move python support to pysh_snakepit.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """
 Shell interpreter written in Python
 """
-import ast
+
 
 from pysh_builtins import BuiltinLauncher
 from pysh_launcher import CommandLauncher
+from pysh_snakepit import SnakeLauncher
 
 
 PATH = '/bin'
@@ -12,29 +13,14 @@ PROMPT = '$ '
 COMMAND = ''
 builtin_launcher = BuiltinLauncher()
 cmd_launcher = CommandLauncher()
-
-
-def run_python(arguments):
-    exec(' '.join(arguments))
-
-
-def is_valid_python_call(arguments):
-    try:
-        tree = ast.parse(arguments)
-        for node in ast.walk(tree):
-            if isinstance(node, ast.Call):
-                return True
-        return False
-    except SyntaxError:
-        return False
-
+snake_launcher = SnakeLauncher()
 
 def execute_cmd(arguments):
     if len(arguments):
         if builtin_launcher.is_builtin(arguments):
             builtin_launcher.launch_builtin(arguments)
-        elif is_valid_python_call(' '.join(arguments)):
-            run_python(arguments)
+        elif snake_launcher.is_valid_python_call(' '.join(arguments)):
+            snake_launcher.run_python(arguments)
         else:
             cmd_launcher.launch_cmd(arguments)
 
