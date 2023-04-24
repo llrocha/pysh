a5fd98c13333143d1f79ea72f42c854d7436b96d
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2023-04-23 22:52:00 -0300

    Remove comments

--- pysh_launcher.py
+++ pysh_launcher.py
@@ -6,6 +6,4 @@ class CommandLauncher():
         if len(arguments):
             process = subprocess.Popen(arguments)
             pid = process.pid
-            print(f'child process, pid: {pid}')
             process.wait()
-            print(f'child process finalized, pid: {pid}')
